5778036d70df2a9bb61964d4c8c9e436e0a3536d
Helios Qiu <admin@lovevup.com>
2025-03-21 23:39:57 +0800

    feat: 增加占卜指令

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
+import os
+import random
+
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.core.message.components import At, Plain, Image
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+folder_path = 'tarot'
+
+
+@register("divine", "Helios", "占卜插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        # 验证目标路径是否为目录
+        if not os.path.isdir(folder_path):
+            logger.error(f"{folder_path} 不是一个有效的文件夹")
+        # 获取目录下所有文件
+        all_files = os.listdir(folder_path)
 
-    async def terminate(self):
-        '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
+        # 过滤出GIF文件（不区分大小写）
+        gif_files = [f for f in all_files if f.lower().endswith('.gif')]
+
+        if not gif_files:
+            logger.error(f"在 {folder_path} 中未找到GIF文件")
+
+        self.gif_files = gif_files
+
+
+
+    # 注册指令的装饰器。指令名为 占卜。
+    @filter.command("占卜")
+    async def helloworld(self, event: AstrMessageEvent):
+        ''' 占卜 '''  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        # 随机选择一个GIF文件
+        selected_gif = random.choice(self.gif_files)
+
+        # 构建完整路径
+        full_path = os.path.abspath(os.path.join(folder_path, selected_gif))
+
+        # 二次验证文件存在性
+
+        if not os.path.isfile(full_path):
+            yield event.plain_result("出错哩！占卜图片缺失......")
+        else:
+            chain = [
+                At(qq=event.get_sender_id()),  # At 消息发送者
+                Plain("占卜结果如下："),
+                Image.fromFileSystem(folder_path),  # 从本地文件目录发送图片
+            ]
+            yield event.chain_result(chain)
